5532a72c7ea39ec7a6569ddd97d8c860e8755199
anthony <theanthonyarellano@gmail.com>
2024-04-07 12:36:26 -0400

    initial dropzone and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,29 @@
-import Link from "next/link";
+"use client";
 
 import styles from "./index.module.css";
+import React from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function Home() {
+  const onDrop = React.useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>
-          Create <span className={styles.pinkSpan}>T3</span> App
+          Run<span className={styles.pinkSpan}>lite</span>
         </h1>
-        <div className={styles.cardRow}>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>First Steps →</h3>
-            <div className={styles.cardText}>
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+        <div {...getRootProps()}>
+          <div>
+            <input {...getInputProps()} />
+            <div className={styles.dropzone}>
+              <div className={styles.dropzoneContainer}>Drop here!</div>
             </div>
-          </Link>
-          <Link
-            className={styles.card}
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className={styles.cardTitle}>Documentation →</h3>
-            <div className={styles.cardText}>
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+          </div>
         </div>
       </div>
     </main>
