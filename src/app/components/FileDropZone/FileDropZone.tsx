09d0ae449ffcf38f39f3637829da5eb86915b87a
anthony <theanthonyarellano@gmail.com>
2024-04-11 18:48:43 -0400

    restrict dropzone to json, move to standalone component

---
+++ src/app/components/FileDropZone/FileDropZone.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "~/app/index.module.css";
+import { useDropzone } from "react-dropzone";
+import { DropzoneFile } from "~/types/Dropzone/DropzoneFile";
+
+export default function FileDropZone() {
+  const onDrop = React.useCallback((acceptedFiles: DropzoneFile[]) => {
+    console.log(acceptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: { "application/json": [".json"] },
+  });
+
+  return (
+    <div {...getRootProps()}>
+      <div>
+        <input {...getInputProps()} />
+        <div className={styles.dropzoneContainer}>Drop or select file</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,16 @@
 
 import React from "react";
 import styles from "./index.module.css";
-import { useDropzone } from "react-dropzone";
-import { DropzoneFile } from "~/types/Dropzone/DropzoneFile";
+import FileDropZone from "./components/FileDropZone/FileDropZone";
 
 export default function Home() {
-
-  const onDrop = React.useCallback((acceptedFiles: DropzoneFile[]) => {
-    console.log(acceptedFiles);
-  }, []);
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.title}>
           run<span className={styles.yellowSpan}>lite</span>
         </h1>
-        <div {...getRootProps()}>
-          <div>
-            <input {...getInputProps()} />
-              <div className={styles.dropzoneContainer}>Drop or select file</div>
-          </div>
-        </div>
+        <FileDropZone />
       </div>
     </main>
   );
