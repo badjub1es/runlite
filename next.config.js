884703c375ed38a0ac6907e15f4c92cc22eeaac4
anthony <theanthonyarellano@gmail.com>
2024-04-13 21:31:32 -0400

    try

--- next.config.js
+++ next.config.js
@@ -3,26 +3,38 @@
  * for Docker builds.
  */
 // /** @type {import('next').NextConfig} */
+// const path = require("path");
+// const stylexPlugin = require("@stylexjs/nextjs-plugin");
+// const babelrc = require("./.babelrc.js");
+// const plugins = babelrc.plugins;
+// const [_name, options] = plugins.find(
+//   (/** @type {string[]} */ plugin) => Array.isArray(plugin) && plugin[0] === "@stylexjs/babel-plugin"
+// );
+// const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
+// const aliases = options.aliases ?? undefined;
+// const useCSSLayers = options.useCSSLayers ?? undefined;
+
+// module.exports = stylexPlugin({ rootDir, aliases, useCSSLayers })({
+//   transpilePackages: ["@stylexjs/open-props"],
+// });
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+/** @type {import('next').NextConfig} */
 const path = require("path");
 const stylexPlugin = require("@stylexjs/nextjs-plugin");
 const babelrc = require("./.babelrc.js");
 const plugins = babelrc.plugins;
-const [_name, options] = plugins.find(
-  (/** @type {string[]} */ plugin) =>
-    Array.isArray(plugin) && plugin[0] === "@stylexjs/babel-plugin"
-);
-const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
-const aliases = options.aliases ?? undefined;
-const useCSSLayers = options.useCSSLayers ?? undefined;
+const [_name, options] =
+  (plugins || []).find(
+    (/** @type {string[]} */ plugin) =>
+      Array.isArray(plugin) && plugin[0] === "@stylexjs/babel-plugin"
+  ) || [];
+const rootDir = options?.unstable_moduleResolution.rootDir ?? __dirname;
+const aliases = options?.aliases ?? undefined;
+const useCSSLayers = options?.useCSSLayers ?? undefined;
 
 module.exports = stylexPlugin({ rootDir, aliases, useCSSLayers })({
   transpilePackages: ["@stylexjs/open-props"],
 });
-
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
