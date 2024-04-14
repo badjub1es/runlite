90d002507d00587ee2296b089457c155c7d2c24c
anthony <theanthonyarellano@gmail.com>
2024-04-13 21:33:50 -0400

    try

--- next.config.js
+++ next.config.js
@@ -7,9 +7,11 @@ const path = require("path");
 const stylexPlugin = require("@stylexjs/nextjs-plugin");
 const babelrc = require("./.babelrc.js");
 const plugins = babelrc.plugins;
-const [_name, options] = plugins.find(
-  (/** @type {string[]} */ plugin) => Array.isArray(plugin) && plugin[0] === "@stylexjs/babel-plugin"
-);
+const [_name, options] =
+  (plugins || []).find(
+    (plugin) =>
+      typeof plugin === "object" && plugin[0] === "@stylexjs/babel-plugin"
+  ) || [];
 const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
 const aliases = options.aliases ?? undefined;
 const useCSSLayers = options.useCSSLayers ?? undefined;
