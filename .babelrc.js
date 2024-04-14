efa0028da885525ec23d425a88f8b1ba1d98588e
anthony <theanthonyarellano@gmail.com>
2024-04-13 20:47:39 -0400

    stylex init

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      '@stylexjs/babel-plugin',
+      {
+        dev: process.env.NODE_ENV === 'development',
+        runtimeInjection: false,
+        genConditionalClasses: true,
+        treeshakeCompensation: true,
+        aliases: {
+          '@/*': [path.join(__dirname, '*')],
+        },
+        unstable_moduleResolution: {
+          type: 'commonJS',
+          rootDir: __dirname,
+        },
+      },
+    ],
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const stylexPlugin = require('@stylexjs/nextjs-plugin');
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +8,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
 
-export default config;
+module.exports = stylexPlugin({
+  aliases: {
+    '@/*': [path.join(__dirname, '*')],
+  },
+  rootDir: __dirname,
+})({});
