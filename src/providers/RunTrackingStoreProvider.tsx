65929dc35561e91d73f1b55726e4eef7ca679a10
badjub1es <theanthonyarellano@gmail.com>
2024-04-21 17:40:21 -0400

    temp

--- src/providers/RunTrackingStoreProvider.tsx
+++ src/providers/RunTrackingStoreProvider.tsx
@@ -7,7 +7,7 @@ import {
   type RunTrackingStore,
   createRunTrackingStore,
   initRunTrackingStore,
-} from "~/stores/RunTrackingStore";
+} from "~/stores/funTrackingStore";
 
 export const RunTrackingStoreContext =
   createContext<StoreApi<RunTrackingStore> | null>(null);
