1c5fdf5ce0600950472c9c9860bb9d47f2ad08a0
badjub1es <theanthonyarellano@gmail.com>
2024-04-24 20:06:55 -0400

    add todo

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Home() {
+  // TODO: If no file is present, redirect to home
   return (
     <section className="background">
       <div>DASHBOARD</div>
