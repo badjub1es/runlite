8956eb7611870880476e494c2dc7f5e936432ef6
anthony <theanthonyarellano@gmail.com>
2024-04-25 21:20:40 -0400

    key into objects with ., alter types

--- src/utils/validateUserFile.ts
+++ src/utils/validateUserFile.ts
@@ -3,56 +3,62 @@ import { DropzoneFile } from "~/types/Dropzone/DropzoneFile";
 import { ErrorMessages } from "~/types/Error/ErrorMessages";
 import { parseJSONFile } from "./parseJSONFile";
 
-const hasOwnPropertyCb = (obj: Map<unknown, unknown>) => {
-    return (key: string) => obj.hasOwnProperty(key);
-}
+const hasOwnPropertyCb = (obj: Partial<UserFile>) => {
+  return (key: string) => obj.hasOwnProperty(key);
+};
 
 /**
  * Determines whether a provided file is valid.
- * 
+ *
  * @param {DropzoneFile} file - DropZone file
  * @returns {ValidateUserFileReturnType} { isValid, error? }
  */
-export const validateUserFile = async (file: DropzoneFile | undefined): Promise<ValidateUserFileReturnType> => {
-    if (file == null) {
-        return { isValid: false, userFile: null };
+export const validateUserFile = async (
+  file: DropzoneFile | undefined
+): Promise<ValidateUserFileReturnType> => {
+  if (file == null) {
+    return { isValid: false, userFile: null };
+  }
+
+  // Parse JSON file to JS object
+  let fileJSON: Partial<UserFile>;
+  try {
+    const parsedFile = await parseJSONFile(file);
+    fileJSON = parsedFile as Partial<UserFile>;
+  } catch (error) {
+    return { isValid: false, error: ErrorMessages.PARSE_ERROR, userFile: null };
+  }
+
+  // Validate JS object to ensure data is in an expected format
+  const check = hasOwnPropertyCb(fileJSON);
+  try {
+    // Name validation
+    if (check("name")) {
+      if (typeof fileJSON.name !== "string") {
+        throw new Error();
+      }
+      if (fileJSON.name.length > 50) {
+        throw new Error();
+      }
+    }
+    if (!check("name")) {
+      throw new Error();
     }
 
-    // Parse JSON file to JS object
-    let fileJSON: Map<unknown, unknown>;
-    try {
-        const parsedFile = await parseJSONFile(file);
-        fileJSON = parsedFile as Map<unknown, unknown>;
-    } catch (error) {
-        return { isValid: false, error: ErrorMessages.PARSE_ERROR, userFile: null }
-    }
-
-    // Validate JS object to ensure data is in an expected format
-    const check = hasOwnPropertyCb(fileJSON);
-    try {
-        // Name validation
-        if (check('name')) {
-            if (typeof fileJSON['name'] !== 'string') {
-                throw new Error();
-            }
-            if (fileJSON['name'].length > 50) {
-                throw new Error();
-            }
-        }
-        if (!check('name')) {
-            throw new Error();
-        }
-
-        // TODO: Continue validations
-        // TODO: Return valid user file
-        return { isValid: true, userFile: null }
-    } catch (error) {
-        return { isValid: false, error: ErrorMessages.FILE_VALIDATION, userFile: null }
-    }
+    // TODO: Continue validations
+    // TODO: Return valid user file
+    return { isValid: true, userFile: null };
+  } catch (error) {
+    return {
+      isValid: false,
+      error: ErrorMessages.FILE_VALIDATION,
+      userFile: null,
+    };
+  }
 };
 
 interface ValidateUserFileReturnType {
-    userFile: UserFile | null;
-    isValid: boolean;
-    error?: string;
+  userFile: UserFile | null;
+  isValid: boolean;
+  error?: string;
 }
