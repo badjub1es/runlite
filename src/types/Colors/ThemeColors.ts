416997cb78a22b9e63b5b14bfdda1fbf0b523a10
anthony <theanthonyarellano@gmail.com>
2024-05-09 19:41:59 -0400

    update colors

--- src/types/Colors/ThemeColors.ts
+++ src/types/Colors/ThemeColors.ts
@@ -2,6 +2,6 @@ export enum ThemeColors {
   PRIMARY = "#101820",
   SECONDARY = "#FEE715",
   GLASS = "rgba(148, 163, 184, 0.2)",
-  WHITE = "#ffffff",
-  ERROR = "red",
+  WHITE = "#FFFFFF",
+  ERROR = "#FF0000",
 }
