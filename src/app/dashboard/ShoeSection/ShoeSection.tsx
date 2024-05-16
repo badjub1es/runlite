d255e7b51d0c91bb169153316117ca79b66022d5
badjub1es <theanthonyarellano@gmail.com>
2024-05-16 18:21:40 -0400

    create modal content container as well as initial create shoe modal

--- src/app/dashboard/ShoeSection/ShoeSection.tsx
+++ src/app/dashboard/ShoeSection/ShoeSection.tsx
@@ -1,20 +1,42 @@
+import React from "react";
 import Card from "~/components/Card/Card";
 import Stack from "~/components/Stack/Stack";
 import CardTitle from "~/components/Card/CardTitle";
 import StyledButton from "~/components/Button/StyledButton";
+import ModalContentContainer from "~/components/Modal/ModalContentContainer";
+import { Fade, Modal } from "@mui/material";
 import { ThemeColors } from "~/types/Colors/ThemeColors";
 
 export default function ShoeSection() {
+  const [modalOpen, setModalOpen] = React.useState(false);
   return (
     <Card backgroundColor={ThemeColors.GLASS}>
       <Stack direction="column" justifyContent="flex-start">
         <Stack justifyContent="space-between">
           <CardTitle color={ThemeColors.WHITE}>Shoes</CardTitle>
-          <StyledButton size="small" variant="contained">
+          <StyledButton
+            size="small"
+            variant="contained"
+            onClick={() => setModalOpen(true)}
+          >
             Create new shoe
           </StyledButton>
         </Stack>
         {/* TODO: Add Shoe UI */}
+        <Modal
+          open={modalOpen}
+          onClose={() => setModalOpen(false)}
+          aria-labelledby="parent-modal-title"
+          aria-describedby="parent-modal-description"
+        >
+          <Fade in={modalOpen}>
+            <div>
+              <ModalContentContainer>
+                <CardTitle id="parent-modal-title">Shoe details</CardTitle>
+              </ModalContentContainer>
+            </div>
+          </Fade>
+        </Modal>
       </Stack>
     </Card>
   );
